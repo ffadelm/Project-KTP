1e3142e6ef9819315bcb03811aa5b0393c8ff3c7
ffadelm <faysalfadelm0107@gmail.com>
2022-04-17 09:50:39 +0700

    create data dummy : error

--- src/main/java/ti/umy/projectKTP/coba/dummyController.java
+++ src/main/java/ti/umy/projectKTP/coba/dummyController.java
@@ -4,12 +4,22 @@
  */
 package ti.umy.projectKTP.coba;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -20,7 +30,7 @@ public class dummyController {
     DummyJpaController dummyController = new DummyJpaController();
     List<Dummy> data = new ArrayList<>();
     
-    @RequestMapping("/create")
+    @RequestMapping("/read")
     @ResponseBody
     public List<Dummy> getDummy( ){
         try{
@@ -28,7 +38,34 @@ public class dummyController {
         }catch(Exception e){
             e.getMessage();
         }
-//        model.addAttribute("goDummy", data);
         return data;
     } 
+    
+    @RequestMapping("/create")
+    public String createDummy(){
+        return "dummy/create";
+    }
+    
+    @PostMapping(value="/newData", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String newDummy(@RequestParam("gambar") MultipartFile file, HttpServletRequest data) throws ParseException, Exception{
+        
+        Dummy dumData = new Dummy();
+        
+        String id = data.getParameter("id");
+        int iid = Integer.parseInt(id);
+        
+        String tanggal = data.getParameter("tanggal");
+        Date date = new SimpleDateFormat("dd-MM-yyyy").parse(tanggal);
+        
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        byte[] image = file.getBytes();
+        
+        dumData.setId(iid);
+        dumData.setTanggal(date);
+        dumData.setGambar(image);
+        
+        dummyController.create(dumData);
+
+        return "dummy/create";
+    }
 }
